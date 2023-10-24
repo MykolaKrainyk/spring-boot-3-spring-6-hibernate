8629cf873bee7a494fa448e301cead4f344e78d9
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-24 20:13:50 +0300

    qualifiers for dependency injection demo

--- 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/example/springcoredemo/controller/DemoController.java
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/example/springcoredemo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.example.springcoredemo.controller;
 
 import com.example.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach coach;
+    @Autowired
+    public DemoController(@Qualifier("footballCoach") Coach coach) {
+        this.coach = coach;
+    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
     }
 
-    @Autowired //any method name will work
-    public void setCoach(Coach coach) {
-        this.coach = coach;
-    }
 }
