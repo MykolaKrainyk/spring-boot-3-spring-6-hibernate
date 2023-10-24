0f0b6b705a80f077e879182e7f1aadd09fb48847
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-24 18:34:41 +0300

    properties demo - custom properties injection

--- 01-spring-boot-overview/03-properties-demo/src/main/java/com/example/springboot/demo/mycoolapp/controller/FunRestController.java
+++ 01-spring-boot-overview/03-properties-demo/src/main/java/com/example/springboot/demo/mycoolapp/controller/FunRestController.java
@@ -1,13 +1,26 @@
 package com.example.springboot.demo.mycoolapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    // inject properties from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    // expose new endpoint for properties injection demo
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
     }
 }
