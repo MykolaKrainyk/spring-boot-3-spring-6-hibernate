1f1eb1db33d40dce74c3452bc85b8dae288adbf3
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-25 14:34:06 +0300

    create and save multiple students for autoincrement id value

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -17,8 +17,20 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("Paul", "Doe", "paul@example.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@example.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@example.com");
+
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
