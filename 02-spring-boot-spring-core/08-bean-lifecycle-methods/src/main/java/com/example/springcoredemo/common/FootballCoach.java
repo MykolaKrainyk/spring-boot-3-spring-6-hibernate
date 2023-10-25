63ec0f331eaf2011abe1c2689ee0299d524ce4d5
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-25 11:23:48 +0300

    add init and destroy methods for bean lifecycle

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/example/springcoredemo/common/FootballCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/example/springcoredemo/common/FootballCoach.java
@@ -1,9 +1,27 @@
 package com.example.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FootballCoach implements Coach {
+
+    public FootballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define init method
+    @PostConstruct
+    public void doMyStartupStaff() {
+        System.out.println("In doMyStartupStaff(): " + getClass().getSimpleName());
+    }
+
+    // define destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
