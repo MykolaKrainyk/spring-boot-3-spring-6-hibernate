0388b8b9548784d44feaab637936e20d87eeb3dd
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-26 12:23:46 +0300

    add EmployeeService and EmployeeController

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.cruddemo.controller;
+
+import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.findAll();
+    }
+}
