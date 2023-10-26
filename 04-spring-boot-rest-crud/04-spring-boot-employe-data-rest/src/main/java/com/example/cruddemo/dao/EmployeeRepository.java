dbee9484bdc887d4f7f36960ac2882e1833cdbec
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-26 14:33:03 +0300

    data-rest some config demo

--- 04-spring-boot-rest-crud/04-spring-boot-employe-data-rest/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-employe-data-rest/src/main/java/com/example/cruddemo/dao/EmployeeRepository.java
@@ -2,8 +2,8 @@ package com.example.cruddemo.dao;
 
 import com.example.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(path = "members") //instead of "/employees" the "/members" will be used
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
