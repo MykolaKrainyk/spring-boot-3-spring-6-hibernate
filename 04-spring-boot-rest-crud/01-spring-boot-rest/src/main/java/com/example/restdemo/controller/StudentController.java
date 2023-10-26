b2271324c42139b3bc8d44835c2a70f8dba9bb95
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-26 10:54:02 +0300

    ExceptionHandler demo

--- 04-spring-boot-rest-crud/01-spring-boot-rest/src/main/java/com/example/restdemo/controller/StudentController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest/src/main/java/com/example/restdemo/controller/StudentController.java
@@ -1,7 +1,12 @@
 package com.example.restdemo.controller;
 
 import com.example.restdemo.entity.Student;
+import com.example.restdemo.entity.StudentErrorResponse;
+import com.example.restdemo.exception.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +37,19 @@ public class StudentController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
