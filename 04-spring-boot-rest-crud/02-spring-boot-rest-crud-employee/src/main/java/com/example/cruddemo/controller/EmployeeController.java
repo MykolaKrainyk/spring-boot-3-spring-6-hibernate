a23397fe2de719a45b724d3ea6ead8fb2e72eb03
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-26 12:58:22 +0300

    add get and post endpoint to EmployeeController, exception handler

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/controller/EmployeeController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/cruddemo/controller/EmployeeController.java
@@ -1,15 +1,23 @@
 package com.example.cruddemo.controller;
 
 import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.exception.EmployeeNotFoundException;
 import com.example.cruddemo.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
@@ -18,8 +26,35 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/employees")
+    @GetMapping()
     public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable("id") int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new EmployeeNotFoundException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping()
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // just in case they pass an id in json ... set id to 0
+        // this is to force a save of new item ... instead of update
+        employee.setId(0);
+
+        return employeeService.save(employee);
+    }
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(EmployeeNotFoundException exception) {
+        ErrorResponse errorResponse = ErrorResponse
+                .builder(exception, HttpStatus.NOT_FOUND, exception.getMessage()).build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
