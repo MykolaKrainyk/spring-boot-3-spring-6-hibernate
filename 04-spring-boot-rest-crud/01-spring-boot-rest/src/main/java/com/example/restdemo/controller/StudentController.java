6bffaa83b6849254d5fed1c3a073b2470d72c511
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-26 10:09:27 +0300

    add student controller

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest/src/main/java/com/example/restdemo/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.restdemo.controller;
+
+import com.example.restdemo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+
+        students.add(new Student("John", "Doe"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+}
