7ac7b2f3551562298abbcfe1c327229785aaa2cd
Mykola Krainyk <mykola_krainyk@epam.com>
2023-11-01 21:48:42 +0200

    mvc form data binding demo

---
+++ 06-spring-boot-spring-mvc/03-form-data-binding/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.thymeleafdemo.controller;
+
+import com.example.thymeleafdemo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/showStudentsForm")
+    public String showForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+        return "student-confirmation";
+    }
+}
