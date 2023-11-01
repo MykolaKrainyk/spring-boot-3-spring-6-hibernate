f1e8a985318efe85ee5f68c60f7e61c925c778cf
Mykola Krainyk <mykola_krainyk@epam.com>
2023-11-01 21:27:53 +0200

    add project for mvc form data binding demo

--- 06-spring-boot-spring-mvc/02-binding-request-param/src/main/java/com/example/thymeleafdemo/controller/DemoController.java
+++ 06-spring-boot-spring-mvc/02-binding-request-param/src/main/java/com/example/thymeleafdemo/controller/DemoController.java
@@ -3,15 +3,16 @@ package com.example.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
 
     @GetMapping("/hello")
-    public String sayHello(Model model) {
-        model.addAttribute("date", new Date());
+    public String sayHello(@RequestParam("studentName") String name, Model model) {
+        String result = "Hey My Friend! " + name.toUpperCase();
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
