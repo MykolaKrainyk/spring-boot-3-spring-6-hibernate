81501940934c4a72516e594eecec42079e8ae977
Mykola Krainyk <mykola_krainyk@epam.com>
2023-11-01 22:06:29 +0200

    mvc form select tag demo

--- 06-spring-boot-spring-mvc/03-form-data-binding/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/03-form-data-binding/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -1,20 +1,26 @@
 package com.example.thymeleafdemo.controller;
 
 import com.example.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${country.list}")
+    private List<String> countriyList;
 
     @GetMapping("/showStudentsForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
-
+        model.addAttribute("countries", countriyList);
         return "student-form";
     }
 
